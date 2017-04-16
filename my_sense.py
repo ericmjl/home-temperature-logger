91e6f265873817e6ee13521b01797eb83b7a4f74
Eric Ma <ericmajinglong@gmail.com>
2017-04-16 10:15:20 -0400

    updated my_sense script

--- my_sense.py
+++ my_sense.py
@@ -1,14 +1,17 @@
-from sense_hat import SenseHat as SH
+from sense_hat import SenseHat
 from time import sleep, now
 from tinydb import TinyDB, where
+from datetime import datetime
 
 db = TinyDB('temperatures.database')
 
-sense = SH()
+sense = SenseHat()
 
 # sense.show_message(str(temperature))
 
 while True:
+    curr_time = datetime.now()
     temperature = sense.get_temperature()
     print(temperature)
+    db.insert({'time': curr_time, 'temperature': temperature})
     sleep(1)
